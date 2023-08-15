4b32ec4b8fac545ee2b0ffc111304a859ca95b65
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2023-08-15 22:45:41 +0300

    add getPayment

--- controllers/getAllPayements.js
+++ controllers/getAllPayements.js
@@ -1,12 +1,14 @@
 const { Payments } = require("../models/Payments");
-const { get } = require("../routes/Routes");
 
 const getAllpayments=async(req,res)=>{
    try{
-
+      const AllPayments=await Payments.find();
+      console.log("all payees",AllPayments);
+      res.status(200).json(AllPayments);
+    
    }catch(e){
     console.error(e.message);
     res.status(500).send("server error")
    }
 }
-module.exports=getAllpayments
+module.exports=getAllpayments;
--- routes/Routes.js
+++ routes/Routes.js
@@ -3,11 +3,11 @@ const router=express.Router();
 
 const {checkTransaction}= require("../controllers/checkTransaction.js")
 const {CreateToken, callBack,stkPush}= require('../controllers/Token.js');
-const { Payments } = require("../models/Payments.js");
+const getAllpayments = require("../controllers/getAllPayements.js");
 router.post("/",CreateToken,stkPush)
 
-router.post('/callback',callBack)
-router.get('/payments',Payments)
+router.post('/callback',callBack);
+router.get('/payments',getAllpayments);
 
 router.get(`/check-transaction/:merchantRequestId`,checkTransaction)
 module.exports=router
